1eb2868f94642e04bc869da6b14e107337815974
fahadkhalid695 <fahadkhalid695@gmail.com>
2025-06-11 19:52:40 +0500

    changes in cert and project

--- Web_Project/src/data/certifications.ts
+++ Web_Project/src/data/certifications.ts
@@ -3,19 +3,19 @@ import { Certification } from '../types';
 export const certifications: Certification[] = [
   {
     id: 1,
-    name: "Python",
-    organization: "Innovista (Learn Easy)",
-    date: "December 2024",
-    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    verificationUrl: "https://www.credly.com/badges/aws-certified-solutions-architect"
-  },
-  {
-    id: 2,
     name: "Cybersecurity Fundamentals",
     organization: "Innovista (Learn Easy)",
     date: "November 2024",
     logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    verificationUrl: "https://www.credly.com/badges/microsoft-certified-azure-fundamentals"
+    verificationUrl: "https://drive.google.com/file/d/1_OJhJetyB-jjGoTSR4JAmuhJPEzhEYk5/view?usp=sharing"
+  },
+  {
+    id: 2,
+    name: "Python",
+    organization: "Innovista (Learn Easy)",
+    date: "December 2024",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://drive.google.com/file/d/1R7bx_ijI_zXNQEhF5TtiMXsocK8eliKC/view?usp=sharing"
   },
   {
     id: 3,
--- Web_Project/src/data/projects.ts
+++ Web_Project/src/data/projects.ts
@@ -7,17 +7,17 @@ export const projects: Project[] = [
     description: "This project explores the sales of different menu items using a dataset. We perform data cleaning, Exploratory Data Analysis (EDA) analyze the sales report of the cafe.",
     image: "/image/cafe_project.png",
     tags: ["Python", "Matplotlib", "Numpy", "Pandas"],
-    githubUrl: "https://github.com/fahadkhalid695/Cafe-Sales-Analysis.git",
-    demoUrl: "https://demo.fahad-khalid.com/image-recognition"
+    githubUrl: "https://github.com/fahadkhalid695/Cafe-Sales-Analysis",
+    demoUrl: "https://colab.research.google.com/drive/1U_nj9m1ZCnZn0YjguzsL2qc8Qz-A4cx1?usp=sharing"
   },
   {
     id: 2,
-    title: "Cloud-Based Task Management System",
-    description: "A serverless task management application built with AWS Lambda, API Gateway, and DynamoDB with real-time notifications.",
-    image: "https://images.pexels.com/photos/7148384/pexels-photo-7148384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    tags: ["AWS", "Serverless", "Node.js", "React"],
-    githubUrl: "https://github.com/fahad-khalid/task-manager",
-    demoUrl: "https://demo.fahad-khalid.com/task-manager"
+    title: "Survial Prediction using Logistic Regression",
+    description: "This project predicts the survival of persons who faced an accident using a dataset. We performed data cleaning, Exploratory Data Analysis (EDA), and trained our model on the data.",
+    image: "/image/lg.png",
+    tags: ["Python", "Logistic Regression", "Numpy", "Scikit-Learn"],
+    githubUrl: "https://github.com/fahadkhalid695/Survival-Prediction-Project",
+    demoUrl: "https://colab.research.google.com/drive/13K8zoV3B0kvA43zT5Kwx-tnn8Z5NN91r?usp=sharing"
   },
   {
     id: 3,
