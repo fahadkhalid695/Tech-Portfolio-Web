6bf104a8c6196512ccd8eba0ec15a96c881cce9c
fahadkhalid695 <fahadkhalid695@gmail.com>
2025-09-16 12:08:51 +0500

    certificate added

--- Web_Project/src/data/certifications.ts
+++ Web_Project/src/data/certifications.ts
@@ -16,5 +16,21 @@ export const certifications: Certification[] = [
     date: "December 2024",
     logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     verificationUrl: "https://drive.google.com/file/d/1R7bx_ijI_zXNQEhF5TtiMXsocK8eliKC/view?usp=sharing"
+  },
+  {
+    id: 2,
+    name: "Python",
+    organization: "Innovista (Learn Easy)",
+    date: "December 2024",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://drive.google.com/file/d/1R7bx_ijI_zXNQEhF5TtiMXsocK8eliKC/view?usp=sharing"
+  },
+  {
+    id: 3,
+    name: "HELM K8s Packaing Manager",
+    organization: "Udemy",
+    date: "September 2025",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://drive.google.com/file/d/1wkceA56si9Fy7h8UxflVPOXmwqp0RoVT/view?usp=sharing"
   }
 ];
