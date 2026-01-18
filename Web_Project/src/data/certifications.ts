601b94d0a7c2078bf662265556c7d9c3abddf419
fahadkhalid695 <fahadkhalid695@gmail.com>
2026-01-18 23:05:38 +0500

    Certificates

--- Web_Project/src/data/certifications.ts
+++ Web_Project/src/data/certifications.ts
@@ -1,6 +1,22 @@
 import { Certification } from '../types';
 
 export const certifications: Certification[] = [
+    {
+    id: 4,
+    name: "Amazon Cloud Foundation",
+    organization: "AWS Academy",
+    date: "January 2026",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://drive.google.com/file/d/1rwnU7mIZZ3ZLgP4bxH8QqehDi3i6VPka/view?usp=sharing"
+  },
+    {
+    id: 5,
+    name: "Baics of Cloud Security",
+    organization: "Google Cloud",
+    date: "January 2026",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://www.skills.google/public_profiles/422b5228-a8eb-4a54-b6b4-6ffd34fcfd3b/badges/21440315"
+  }
   {
     id: 1,
     name: "Cybersecurity Fundamentals",
@@ -17,6 +33,22 @@ export const certifications: Certification[] = [
     logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     verificationUrl: "https://drive.google.com/file/d/1R7bx_ijI_zXNQEhF5TtiMXsocK8eliKC/view?usp=sharing"
   },
+    {
+    id: 6,
+    name: "App Dev using Copilot",
+    organization: "Microsoft",
+    date: "December 2025",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://learn.microsoft.com/api/credentials/share/en-us/fahadkhalid695/6EC762C274CFC226?sharingId=5FF9E4503AB95564"
+  },
+    {
+    id: 7,
+    name: "Gemini for Application Developer",
+    organization: "Google Cloud",
+    date: "January 2026",
+    logo: "https://images.pexels.com/photos/11343088/pexels-photo-11343088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    verificationUrl: "https://www.skills.google/public_profiles/422b5228-a8eb-4a54-b6b4-6ffd34fcfd3b/badges/21330858"
+  }
   {
     id: 3,
     name: "HELM k8s Packaing Manager",
